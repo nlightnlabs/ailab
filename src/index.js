980fa4eeb8ee425a8cb959c7d1e7e7a50a6f40a5
nlightnlabs <@nlightnlabs.com>
2024-05-30 19:56:19 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import './mainStyle.css'
+import App from './App'
+import Home from "./Home"
+import SignIn from "./modules/authentication/modules/SignIn"
+import SignUp from "./modules/authentication/modules/SignUp"
+import ResetPassword from "./modules/authentication/modules/ResetPassword"
+
+// As of React 18
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+console.log("ailab is running")
+console.log("Environment: ",process.env.NODE_ENV)
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="/Home" element={<Home />}/>
+        <Route path="/SignIn" element={<SignIn />}/>
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
+      </Routes>
+    </BrowserRouter>
+    </PersistGate>
+  </Provider>
+)
 
